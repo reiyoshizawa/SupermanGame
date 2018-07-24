6b4be5d0c0015d3531d07071c899e4a1d1d6f897
Rei Yoshizawa <reiyoshizawa0418@gmail.com>
2018-07-23 22:37:23 -0700

    add log in menu

--- app/src/main/java/com/example/yoshizawarei/supermangame/BgmPlayer.java
+++ app/src/main/java/com/example/yoshizawarei/supermangame/BgmPlayer.java
@@ -9,12 +9,8 @@ public class BgmPlayer {
 
     public BgmPlayer(Context context) {
         this.mediaPlayer = MediaPlayer.create(context, R.raw.bgm);
-
         this.mediaPlayer.setLooping(true);
-
         this.mediaPlayer.setVolume(1.0f, 1.0f);
-
-//        this.mediaPlayer.attachAuxEffect(1);
     }
 
     public void start() {
---
+++ app/src/main/java/com/example/yoshizawarei/supermangame/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.yoshizawarei.supermangame;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
+    private EditText email_et;
+    private EditText pw_et;
+    private Button login_bottun;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        email_et = findViewById(R.id.email_et);
+        pw_et = findViewById(R.id.pw_et);
+        login_bottun = findViewById(R.id.login_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        login_bottun.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String email = email_et.getText().toString();
+        String password = pw_et.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, start.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+}
